a4b9c457fcd0d402c963eadad5042ffb79c4dbe2
ravi <artiveloper@gmail.com>
2020-08-03 17:58:36 +0900

    프로필 페이지 만들기

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
+import FollowList from '../components/FollowList';
+import NicknameEditForm from '../components/NicknameEditForm';
 
 const Profile = () => {
+
+    const followers = [{nickname: '제로초'}, {nickname: '제로초2'}, {nickname: '제로초3'}]
+    const followings = [{nickname: '제로초'}, {nickname: '제로초2'}, {nickname: '제로초3'}]
+
     return (
         <>
             <Head>
-                <title>NodeBird | 프로필</title>
+                <title>프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>프로필 페이지</div>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followings}/>
+                <FollowList header="팔로워 목록" data={followers}/>
             </AppLayout>
         </>
     )
