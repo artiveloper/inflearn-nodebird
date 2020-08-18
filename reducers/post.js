ef0576b485e7aca2f5d7dab31a6d515b6b6f1dcd
ravi <artiveloper@gmail.com>
2020-08-18 14:13:51 +0900

    immer 도입

--- reducers/post.js
+++ reducers/post.js
@@ -1,4 +1,5 @@
 import shortId from 'shortid'
+import produce from 'immer'
 
 export const initialState = {
     mainPosts: [
@@ -103,92 +104,69 @@ const dummyComment = (data) => ({
     },
 })
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => produce(state, (draft) => {
     switch (action.type) {
     // 게시글 등록
     case ADD_POST_REQUEST: {
-        return {
-            ...state,
-            addPostLoading: true,
-            addPostDone: false,
-            addPostError: null,
-        }
+        draft.addPostLoading = true
+        draft.addPostDone = true
+        draft.addPostError = null
+        break
     }
     case ADD_POST_SUCCESS: {
-        return {
-            ...state,
-            mainPosts: [dummyPost(action.data), ...state.mainPosts],
-            addPostLoading: false,
-            addPostDone: true,
-        }
+        draft.mainPosts.unshift(dummyPost(action.data))
+        draft.addPostLoading = false
+        draft.addPostDone = true
+        break
     }
     case ADD_POST_FAILURE: {
-        return {
-            ...state,
-            addPostLoading: false,
-            addPostError: action.error,
-        }
+        draft.addPostLoading = false
+        draft.addPostError = action.error
+        break
     }
 
     // 게시글 삭제
     case REMOVE_POST_REQUEST: {
-        return {
-            ...state,
-            removePostLoading: true,
-            removePostDone: false,
-            removePostError: null,
-        }
+        draft.removePostLoading = true
+        draft.removePostDone = false
+        draft.removePostError = null
+        break
     }
     case REMOVE_POST_SUCCESS: {
-        return {
-            ...state,
-            mainPosts: state.mainPosts.filter((p) => p.id !== action.data),
-            removePostLoading: false,
-            removePostDone: true,
-        }
+        draft.mainPosts = draft.mainPosts.filter((p) => p.id !== action.data)
+        draft.removePostLoading = false
+        draft.removePostDone = true
+        break
     }
     case REMOVE_POST_FAILURE: {
-        return {
-            ...state,
-            removePostLoading: false,
-            removePostError: action.error,
-        }
+        draft.removePostLoading = false
+        draft.removePostError = action.error
+        break
     }
 
     // 댓글 등록
     case ADD_COMMENT_REQUEST: {
-        return {
-            ...state,
-            addCommentLoading: true,
-            addCommentDone: false,
-            addCommentError: null,
-        }
+        draft.addCommentLoading = true
+        draft.addCommentDone = false
+        draft.addCommentError = null
+        break
     }
     case ADD_COMMENT_SUCCESS: {
-        const postIndex = state.mainPosts.findIndex((p) => p.id === action.data.postId)
-        const post = { ...state.mainPosts[postIndex] }
-        post.Comments = [dummyComment(action.data.content), ...post.Comments]
-        const mainPosts = [...state.mainPosts]
-        mainPosts[postIndex] = post
-
-        return {
-            ...state,
-            mainPosts,
-            addCommentLoading: false,
-            addCommentDone: true,
-        }
+        const post = draft.mainPosts.find((p) => p.id === action.data.postId)
+        post.Comments.unshift(dummyComment(action.data.content))
+        draft.addCommentLoading = false
+        draft.addCommentDone = true
+        break
     }
     case ADD_COMMENT_FAILURE: {
-        return {
-            ...state,
-            addCommentLoading: false,
-            addCommentError: action.error,
-        }
+        draft.addCommentLoading = false
+        draft.addCommentError = action.error
+        break
     }
 
     default:
-        return state
+        break
     }
-}
+})
 
 export default reducer
--- reducers/user.js
+++ reducers/user.js
@@ -1,4 +1,8 @@
-import { ADD_POST_TO_ME, REMOVE_POST_TO_ME } from './post'
+import produce from 'immer'
+import {
+    ADD_POST_TO_ME,
+    REMOVE_POST_TO_ME,
+} from './post'
 
 export const initialState = {
     logInLoading: false, // 로그인
@@ -64,127 +68,91 @@ export const logoutRequestAction = () => ({
     type: LOG_OUT_REQUEST,
 })
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => produce(state, (draft) => {
     switch (action.type) {
     // 로그인
-    case LOG_IN_REQUEST: {
-        return {
-            ...state,
-            logInLoading: true,
-            logInDone: false,
-            logInError: null,
-        }
-    }
-    case LOG_IN_SUCCESS: {
-        return {
-            ...state,
-            logInLoading: false,
-            logInDone: true,
-            me: dummyUser(action.data),
-        }
-    }
-    case LOG_IN_FAILURE: {
-        return {
-            ...state,
-            logInLoading: false,
-            logInError: action.error,
-        }
-    }
+    case LOG_IN_REQUEST:
+        draft.logInLoading = true
+        draft.logInDone = false
+        draft.logInError = null
+        break
+
+    case LOG_IN_SUCCESS:
+        draft.logInLoading = false
+        draft.logInDone = true
+        draft.me = dummyUser(action.data)
+        break
+
+    case LOG_IN_FAILURE:
+        draft.logInLoading = false
+        draft.logInError = action.error
+        break
 
     // 로그아웃
-    case LOG_OUT_REQUEST: {
-        return {
-            ...state,
-            logOutLoading: true,
-            logOutDone: false,
-            logOutError: null,
-        }
-    }
-    case LOG_OUT_SUCCESS: {
-        return {
-            ...state,
-            logOutLoading: false,
-            logOutDone: true,
-            me: null,
-        }
-    }
-    case LOG_OUT_FAILURE: {
-        return {
-            ...state,
-            logOutLoading: false,
-            logOutError: action.error,
-        }
-    }
+    case LOG_OUT_REQUEST:
+        draft.logOutLoading = true
+        draft.logOutDone = false
+        draft.logOutError = null
+        break
+
+    case LOG_OUT_SUCCESS:
+        draft.logOutLoading = false
+        draft.logOutDone = true
+        draft.me = null
+        break
+
+    case LOG_OUT_FAILURE:
+        draft.logOutLoading = false
+        draft.logOutError = action.error
+        break
 
     // 회원가입
-    case SIGN_UP_REQUEST: {
-        return {
-            ...state,
-            signUpLoading: true,
-            signUpDone: false,
-            signUpError: null,
-        }
-    }
-    case SIGN_UP_SUCCESS: {
-        return {
-            ...state,
-            signUpLoading: false,
-            signUpDone: true,
-            me: null,
-        }
-    }
-    case SIGN_UP_FAILURE: {
-        return {
-            ...state,
-            signUpLoading: false,
-            signUpError: action.error,
-        }
-    }
+    case SIGN_UP_REQUEST:
+        draft.signUpLoading = true
+        draft.signUpDone = false
+        draft.signUpError = null
+        break
+
+    case SIGN_UP_SUCCESS:
+        draft.signUpLoading = false
+        draft.signUpDone = true
+        draft.me = null
+        break
+
+    case SIGN_UP_FAILURE:
+        draft.signUpLoading = false
+        draft.signUpError = action.error
+        break
 
     // 닉네임 변경
-    case CHANGE_NICKNAME_REQUEST: {
-        return {
-            ...state,
-            changeNicknameLoading: true,
-            changeNicknameDone: false,
-            changeNicknameError: null,
-        }
-    }
-    case CHANGE_NICKNAME_SUCCESS: {
-        return {
-            ...state,
-            changeNicknameLoading: false,
-            changeNicknameDone: true,
-            me: null,
-        }
-    }
-    case CHANGE_NICKNAME_FAILURE: {
-        return {
-            ...state,
-            changeNicknameLoading: false,
-            changeNicknameError: action.error,
-        }
-    }
+    case CHANGE_NICKNAME_REQUEST:
+        draft.changeNicknameLoading = true
+        draft.changeNicknameDone = false
+        draft.changeNicknameError = null
+        break
+
+    case CHANGE_NICKNAME_SUCCESS:
+        draft.changeNicknameLoading = false
+        draft.changeNicknameDone = true
+        draft.me = null
+        break
+
+    case CHANGE_NICKNAME_FAILURE:
+        draft.changeNicknameLoading = false
+        draft.changeNicknameError = action.error
+        break
+
     case ADD_POST_TO_ME:
-        return {
-            ...state,
-            me: {
-                ...state.me,
-                Posts: [{ id: action.data }, ...state.me.Posts],
-            },
-        }
+        draft.me.Posts.unshift({ id: action.data })
+        break
+
     case REMOVE_POST_TO_ME:
-        return {
-            ...state,
-            me: {
-                ...state.me,
-                Posts: state.me.Posts.filter((p) => p.id !== action.data),
-            },
-        }
+        draft.me.Posts = draft.me.Posts.filter((p) => p.id !== action.data)
+        break
 
     default:
-        return state
+        break
     }
-}
+})
 
 export default reducer
